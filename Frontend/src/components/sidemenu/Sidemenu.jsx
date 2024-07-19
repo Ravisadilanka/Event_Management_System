86cf37679a9ad2686389c7a862fcc3343a4dc6f1
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-07-19 19:58:57 +0530

    Updated sidemenu

--- Frontend/src/components/sidemenu/Sidemenu.jsx
+++ Frontend/src/components/sidemenu/Sidemenu.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import './Sidemenu.css';
+import "./Sidemenu.css";
+import { FaHome, FaPlusCircle, FaUserPlus } from "react-icons/fa";
 
 const Sidemenu = () => {
   return (
     <div className="sidemenu-container">
-        <div className="sidemenu-header">
+      <div className="sidemenu-header">
         <h3>Event Master</h3>
         <p>DASHBOARD</p>
+      </div>
+      <div className="sidemenu-items">
+        <ul className="sidemenu-list">
+          <li className="sidemenu-item">
+            <FaHome className="sidemenu-icon" />
+            View Events
+          </li>
+          <li className="sidemenu-item">
+            <FaPlusCircle className="sidemenu-icon" />
+            Add Events
+          </li>
+          <li className="sidemenu-item">
+            <FaUserPlus className="sidemenu-icon" />
+            Register Attendee
+          </li>
+        </ul>
+        <div className="sidemenu-profile">
+          <img src="profile-pic-url" alt="Profile" className="profile-pic" />
+          <p className="profile-name">Ravisa Dilanka</p>
         </div>
-        <div className="sidemenu-items">
-            <ul className="sidemenu-list">
-                <li>View Events</li>
-                <li>Add Events</li>
-                <li>Register Attendee</li>
-            </ul>
-        </div>
+      </div>
     </div>
   );
 };
